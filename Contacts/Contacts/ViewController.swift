40b2dc84d4fafef96b6608ffc3d49dd9fbdb39c7
Thinh Tran <thinhtran@outlook.com.au>
2019-10-07 20:01:47 +0800

    ini

---
+++ Contacts/Contacts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Contacts
+//
+//  Created by Thinh on 7/10/19.
+//  Copyright © 2019 VisualSharp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
